36fc90031a630bd4388833e82a4e8cde956a70eb
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2021-10-20 14:04:10 +0200

    Update sample code version

---
+++ SDL_Platformer/Game/Source/App.cpp
@@ -0,0 +1,318 @@
+#include "App.h"
+#include "Window.h"
+#include "Input.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Scene.h"
+#include "Map.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#include <iostream>
+#include <sstream>
+
+// Constructor
+App::App(int argc, char* args[]) : argc(argc), args(args)
+{
+	frames = 0;
+
+	win = new Window();
+	input = new Input();
+	render = new Render();
+	tex = new Textures();
+	audio = new Audio();
+	scene = new Scene();
+	map = new Map();
+
+	// Ordered for awake / Start / Update
+	// Reverse order of CleanUp
+	AddModule(win);
+	AddModule(input);
+	AddModule(tex);
+	AddModule(audio);
+	AddModule(scene);
+	AddModule(map);
+
+	// Render last to swap buffer
+	AddModule(render);
+}
+
+// Destructor
+App::~App()
+{
+	// Release modules
+	ListItem<Module*>* item = modules.end;
+
+	while(item != NULL)
+	{
+		RELEASE(item->data);
+		item = item->prev;
+	}
+
+	modules.clear();
+}
+
+void App::AddModule(Module* module)
+{
+	module->Init();
+	modules.add(module);
+}
+
+// Called before render is available
+bool App::Awake()
+{
+	pugi::xml_document configFile;
+	pugi::xml_node config;
+	pugi::xml_node configApp;
+
+	bool ret = false;
+
+	// L01: DONE 3: Load config from XML
+	config = LoadConfig(configFile);
+
+	if (config.empty() == false)
+	{
+		ret = true;
+		configApp = config.child("app");
+
+		// L01: DONE 4: Read the title from the config file
+		title.Create(configApp.child("title").child_value());
+		organization.Create(configApp.child("organization").child_value());
+	}
+
+	if (ret == true)
+	{
+		ListItem<Module*>* item;
+		item = modules.start;
+
+		while ((item != NULL) && (ret == true))
+		{
+			// L01: DONE 5: Add a new argument to the Awake method to receive a pointer to an xml node.
+			// If the section with the module name exists in config.xml, fill the pointer with the valid xml_node
+			// that can be used to read all variables for that module.
+			// Send nullptr if the node does not exist in config.xml
+			ret = item->data->Awake(config.child(item->data->name.GetString()));
+			item = item->next;
+		}
+	}
+
+	return ret;
+}
+
+// Called before the first frame
+bool App::Start()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.start;
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->Start();
+		item = item->next;
+	}
+
+	return ret;
+}
+
+// Called each loop iteration
+bool App::Update()
+{
+	bool ret = true;
+	PrepareUpdate();
+
+	if(input->GetWindowEvent(WE_QUIT) == true)
+		ret = false;
+
+	if(ret == true)
+		ret = PreUpdate();
+
+	if(ret == true)
+		ret = DoUpdate();
+
+	if(ret == true)
+		ret = PostUpdate();
+
+	FinishUpdate();
+	return ret;
+}
+
+// Load config from XML file
+// NOTE: Function has been redesigned to avoid storing additional variables on the class
+pugi::xml_node App::LoadConfig(pugi::xml_document& configFile) const
+{
+	pugi::xml_node ret;
+
+	pugi::xml_parse_result result = configFile.load_file(CONFIG_FILENAME);
+
+	if (result == NULL) LOG("Could not load xml file: %s. pugi error: %s", CONFIG_FILENAME, result.description());
+	else ret = configFile.child("config");
+
+	return ret;
+}
+
+// ---------------------------------------------
+void App::PrepareUpdate()
+{
+}
+
+// ---------------------------------------------
+void App::FinishUpdate()
+{
+	// L02: DONE 1: This is a good place to call Load / Save methods
+	if (loadGameRequested == true) LoadGame();
+	if (saveGameRequested == true) SaveGame();
+}
+
+// Call modules before each loop iteration
+bool App::PreUpdate()
+{
+	bool ret = true;
+
+	ListItem<Module*>* item;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->PreUpdate();
+	}
+
+	return ret;
+}
+
+// Call modules on each loop iteration
+bool App::DoUpdate()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.start;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->Update(dt);
+	}
+
+	return ret;
+}
+
+// Call modules after each loop iteration
+bool App::PostUpdate()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	Module* pModule = NULL;
+
+	for(item = modules.start; item != NULL && ret == true; item = item->next)
+	{
+		pModule = item->data;
+
+		if(pModule->active == false) {
+			continue;
+		}
+
+		ret = item->data->PostUpdate();
+	}
+
+	return ret;
+}
+
+// Called before quitting
+bool App::CleanUp()
+{
+	bool ret = true;
+	ListItem<Module*>* item;
+	item = modules.end;
+
+	while(item != NULL && ret == true)
+	{
+		ret = item->data->CleanUp();
+		item = item->prev;
+	}
+
+	return ret;
+}
+
+// ---------------------------------------
+int App::GetArgc() const
+{
+	return argc;
+}
+
+// ---------------------------------------
+const char* App::GetArgv(int index) const
+{
+	if(index < argc)
+		return args[index];
+	else
+		return NULL;
+}
+
+// ---------------------------------------
+const char* App::GetTitle() const
+{
+	return title.GetString();
+}
+
+// ---------------------------------------
+const char* App::GetOrganization() const
+{
+	return organization.GetString();
+}
+
+// Load / Save
+void App::LoadGameRequest()
+{
+	// NOTE: We should check if SAVE_STATE_FILENAME actually exist
+	loadGameRequested = true;
+}
+
+// ---------------------------------------
+void App::SaveGameRequest() const
+{
+	// NOTE: We should check if SAVE_STATE_FILENAME actually exist and... should we overwriten
+	saveGameRequested = true;
+}
+
+// ---------------------------------------
+// L02: TODO 5: Create a method to actually load an xml file
+// then call all the modules to load themselves
+bool App::LoadGame()
+{
+	bool ret = false;
+
+	//...
+
+	loadGameRequested = false;
+
+	return ret;
+}
+
+// L02: TODO 7: Implement the xml save method for current state
+bool App::SaveGame() const
+{
+	bool ret = true;
+
+	//...
+
+	saveGameRequested = false;
+
+	return ret;
+}
+
+
+
---
+++ SDL_Platformer/Game/Source/Scene.cpp
@@ -0,0 +1,105 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Map.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	// L03: DONE: Load map
+	//app->map->Load("hello.tmx");
+	app->map->Load("iso_walk.tmx");
+	
+	// Load music
+	app->audio->PlayMusic("Assets/audio/music/music_spy.ogg");
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+    // L02: DONE 3: Request Load / Save when pressing L/S
+	if(app->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		app->LoadGameRequest();
+
+	if(app->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		app->SaveGameRequest();
+
+	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		app->render->camera.y -= 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		app->render->camera.y += 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		app->render->camera.x -= 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		app->render->camera.x += 1;
+
+	//app->render->DrawTexture(img, 380, 100); // Placeholder not needed any more
+
+	// Draw map
+	app->map->Draw();
+
+	// L03: DONE 7: Set the window title with map/tileset info
+	SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
+				   app->map->mapData.width, app->map->mapData.height,
+				   app->map->mapData.tileWidth, app->map->mapData.tileHeight,
+				   app->map->mapData.tilesets.count());
+
+	app->win->SetTitle(title.GetString());
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
