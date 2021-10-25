8ed2e0a7ddc9dbdc3bfcfd39a8818b86c4907d6a
David <79161120+Divangus@users.noreply.github.com>
2021-10-25 12:16:40 +0200

    si

---
+++ SDL_Platformer/Output/Assets/maps/TerrainAssets1.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Snack_machine_Bench_Trash_bin (16 x 16)" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="Snack_machine_Bench_Trash_bin (16 x 16).png" width="128" height="64"/>
+ <wangsets>
+  <wangset name="Banco y basura" type="edge" tile="20">
+   <wangcolor name="Esteban" color="#00ff00" tile="20" probability="1"/>
+   <wangcolor name="Pulsa Ctrl" color="#00ff00" tile="22" probability="1"/>
+   <wangtile tileid="19" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="20" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="21" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="22" wangid="2,0,2,0,2,0,2,0"/>
+  </wangset>
+  <wangset name="maquina espendedora" type="corner" tile="9">
+   <wangcolor name="Cacaolat" color="#ff0000" tile="10" probability="1"/>
+   <wangtile tileid="9" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="10" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
