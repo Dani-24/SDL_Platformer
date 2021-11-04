33995942a5f239e71408d3d5766e02baf47af42c
David <79161120+Divangus@users.noreply.github.com>
2021-11-04 21:14:54 +0100

    Create Blocks (16 x 16).tsx

---
+++ SDL_Platformer/Output/Assets/maps/Blocks (16 x 16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Blocks (16 x 16)" tilewidth="16" tileheight="16" tilecount="66" columns="11">
+ <image source="Blocks (16 x 16).png" width="176" height="96"/>
+</tileset>
