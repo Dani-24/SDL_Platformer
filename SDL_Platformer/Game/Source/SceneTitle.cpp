4d7d6af6ea77512aba5f21e020b06bce54121aab
Dani-24 <dani.toledo.23@hotmail.com>
2021-11-07 21:40:01 +0100

    Title Scene y ajustes de Logo Scene

--- SDL_Platformer/Game/Source/SceneTitle.cpp
+++ SDL_Platformer/Game/Source/SceneTitle.cpp
@@ -30,6 +30,23 @@ bool SceneTitle::Awake()
 // Called before the first frame
 bool SceneTitle::Start()
 {
+	// Textures
+	bg = app->tex->Load("Assets/maps/Background parts/_Complete_static_BG_(288 x 208).png");
+	bgSky = app->tex->Load("Assets/maps/Background parts/5 - Sky_color.png");
+	scroller[0] = 0;
+	scroller[1] = 288;
+	scroller[2] = 288 * 2;
+	scroller[3] = 288 * 3;
+
+	titleText = app->tex->Load("Assets/textures/title.png");
+	titleY = -200;
+	pressEnter = app->tex->Load("Assets/textures/title2.png");
+
+	// Audio
+	app->audio->PlayMusic("Assets/audio/music/music_title.ogg");
+
+	fxEnter = app->audio->LoadFx("Assets/audio/fx/enter.wav");
+
 	return true;
 }
 
@@ -43,7 +60,18 @@ bool SceneTitle::PreUpdate()
 bool SceneTitle::Update(float dt)
 {
 	// De momento no hay nada asi q pasa a la scene
-	app->fade->StartFadeToBlack(this, (Module*)app->scene, 0);
+	if (app->input->GetKey(SDL_SCANCODE_RETURN) == KEY_DOWN) {
+		app->audio->PlayFx(fxEnter);
+		app->fade->StartFadeToBlack(this, (Module*)app->scene, 0);
+	}
+
+	if (titleY < 50) {
+		titleY += 1;
+		titleMove = true;
+	}
+	else {
+		titleMove = false;
+	}
 
 	return true;
 }
@@ -53,6 +81,30 @@ bool SceneTitle::PostUpdate()
 {
 	bool ret = true;
 
+	// --- Draw stuff ---
+
+	// Background
+
+	for (int i = 0; i < 4; i++) {
+		if (scroller[i] <= -288) {
+			scroller[i] = 288 * 3;
+		}
+		else {
+			scroller[i] -= 0.5f;
+		}
+		app->render->DrawTexture(bg, scroller[i], 158);
+
+		// Sky
+		app->render->DrawTexture(bgSky, 288 * i, -50);
+	}
+
+	// Title
+	app->render->DrawTexture(titleText, 180, titleY);
+
+	if (titleMove == false) {
+		app->render->DrawTexture(pressEnter, 220, titleY + 150);
+	}
+
 	return ret;
 }
 
@@ -60,6 +112,13 @@ bool SceneTitle::PostUpdate()
 bool SceneTitle::CleanUp()
 {
 	LOG("Cleaning Title Scene");
+
+	app->tex->UnLoad(bg);
+	app->tex->UnLoad(bgSky);
+	app->tex->UnLoad(titleText);
+	app->tex->UnLoad(pressEnter);
+
+	fxEnter = 0;
 
 	return true;
 }
