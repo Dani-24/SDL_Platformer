c8a03a1029b94bb4735029247c0e125fd13ebc2b
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2021-11-29 11:41:27 +0100

    AAAAAAAAAAA

--- SDL_Platformer/Game/Source/Player.cpp
+++ SDL_Platformer/Game/Source/Player.cpp
@@ -20,28 +20,13 @@ Player::~Player()
 
 void Player::PlayerStartAnims() {
 
-	//player
+	// player RIGHT
 	playerR.PushBack({ 0, 0, 32, 32 });
 
-	//attack ->
-	punchR.PushBack({ 0,32,32,32 });
-	punchR.PushBack({ 32,32,32,32 });
-	punchR.PushBack({ 64,32,32,32 });
-	punchR.PushBack({ 96,32,32,32 });
-	punchR.loop = false;
-	punchR.speed = 0.1;
-
-	//doble attack ->
-	doublePunchR.PushBack({ 0,64,32,32 });
-	doublePunchR.PushBack({ 32,64,32,32 });
-	doublePunchR.PushBack({ 64,64,32,32 });
-	doublePunchR.PushBack({ 96,64,32,32 });
-	doublePunchR.PushBack({ 128,64,32,32 });
-	doublePunchR.PushBack({ 160,64,32,32 });
-	doublePunchR.loop = true;
-	doublePunchR.speed = 0.1;
-
-	//climb
+	// player LEFT
+	playerL.PushBack({ 224, 416, 32, 32 });
+	
+	// CLIMB
 	climb.PushBack({ 0,96,32,32 });
 	climb.PushBack({ 32,96,32,32 });
 	climb.PushBack({ 64,96,32,32 });
@@ -49,7 +34,7 @@ void Player::PlayerStartAnims() {
 	climb.loop = true;
 	climb.speed = 0.3;
 
-	//morir ->
+	// DIE
 	deathR.PushBack({ 0,128,32,32 });
 	deathR.PushBack({ 32,128,32,32 });
 	deathR.PushBack({ 64,128,32,32 });
@@ -61,15 +46,7 @@ void Player::PlayerStartAnims() {
 	deathR.loop = true;
 	deathR.speed = 0.3;
 
-	//hit ->
-	hitR.PushBack({ 0,160,32,32 });
-	hitR.PushBack({ 32,160,32,32 });
-	hitR.PushBack({ 64,160,32,32 });
-	hitR.PushBack({ 96,160,32,32 });
-	hitR.loop = true;
-	hitR.speed = 0.3;
-
-	//idle ->
+	// idle RIGHT
 	idleR.PushBack({ 0,192,32,32 });
 	idleR.PushBack({ 32,192,32,32 });
 	idleR.PushBack({ 64,192,32,32 });
@@ -77,14 +54,14 @@ void Player::PlayerStartAnims() {
 	idleR.loop = true;
 	idleR.speed = 0.1;
 
-	//jump ->
-	jumpR.PushBack({ 0,224,32,32 });
-	jumpR.PushBack({ 32,224,32,32 });
-	jumpR.PushBack({ 64,224,32,32 });
-	jumpR.PushBack({ 96,224,32,32 });
-	jumpR.PushBack({ 128,224,32,32 });
-	jumpR.loop = false;
-	jumpR.speed = 0.2;
+	//idle LEFT
+	idleL.PushBack({ 224,608,32,32 });
+	idleL.PushBack({ 192,608,32,32 });
+	idleL.PushBack({ 160,608,32,32 });
+	idleL.PushBack({ 128,608,32,32 });
+	idleL.loop = true;
+	idleL.speed = 0.1;
+
 
 	fallR.PushBack({ 160,224,32,32 });
 	fallR.PushBack({ 192,224,32,32 });
@@ -92,7 +69,14 @@ void Player::PlayerStartAnims() {
 	fallR.loop = false;
 	fallR.speed = 0.2;
 
-	//push ->
+	// fall LEFT
+	fallL.PushBack({ 64,640,32,32 });
+	fallL.PushBack({ 32,640,32,32 });
+	fallL.PushBack({ 0,640,32,32 });
+	fallL.loop = true;
+	fallL.speed = 0.3;
+
+	//push RIGHT
 	pushR.PushBack({ 0,256,32,32 });
 	pushR.PushBack({ 32,256,32,32 });
 	pushR.PushBack({ 64,256,32,32 });
@@ -101,8 +85,18 @@ void Player::PlayerStartAnims() {
 	pushR.PushBack({ 160,256,32,32 });
 	pushR.loop = true;
 	pushR.speed = 0.3;
+	
+	//push LEFT
+	pushL.PushBack({ 224,672,32,32 });
+	pushL.PushBack({ 192,672,32,32 });
+	pushL.PushBack({ 160,672,32,32 });
+	pushL.PushBack({ 128,672,32,32 });
+	pushL.PushBack({ 96,672,32,32 });
+	pushL.PushBack({ 64,672,32,32 });
+	pushL.loop = true;
+	pushL.speed = 0.3;
 
-	//run ->
+	//run RIGHT
 	runR.PushBack({ 0,288,32,32 });
 	runR.PushBack({ 32,288,32,32 });
 	runR.PushBack({ 64,288,32,32 });
@@ -111,8 +105,18 @@ void Player::PlayerStartAnims() {
 	runR.PushBack({ 160,288,32,32 });
 	runR.loop = true;
 	runR.speed = 0.2;
+	
+	//run LEFT
+	runL.PushBack({ 224,704,32,32 });
+	runL.PushBack({ 192,704,32,32 });
+	runL.PushBack({ 160,704,32,32 });
+	runL.PushBack({ 128,704,32,32 });
+	runL.PushBack({ 96,704,32,32 });
+	runL.PushBack({ 64,704,32,32 });
+	runL.loop = true;
+	runL.speed = 0.2;
 
-	//throw ->
+	//throw RIGHT
 	throwR.PushBack({ 0,320,32,32 });
 	throwR.PushBack({ 32,320,32,32 });
 	throwR.PushBack({ 64,320,32,32 });
@@ -120,7 +124,15 @@ void Player::PlayerStartAnims() {
 	throwR.loop = true;
 	throwR.speed = 0.3;
 
-	//Walk right -> animation
+	//throw LEFT
+	throwL.PushBack({ 224,736,32,32 });
+	throwL.PushBack({ 192,736,32,32 });
+	throwL.PushBack({ 160,736,32,32 });
+	throwL.PushBack({ 128,736,32,32 });
+	throwL.loop = true;
+	throwL.speed = 0.3;
+
+	//Walk right -> animation RIGHT
 	walkR.PushBack({ 0,352,32,32 });
 	walkR.PushBack({ 32,352,32,32 });
 	walkR.PushBack({ 64,352,32,32 });
@@ -130,7 +142,17 @@ void Player::PlayerStartAnims() {
 	walkR.loop = true;
 	walkR.speed = 0.1;
 
-	//walk and punch ->
+	//Walk LEFT animation
+	walkL.PushBack({ 224,768,32,32 });
+	walkL.PushBack({ 192,768,32,32 });
+	walkL.PushBack({ 160,768,32,32 });
+	walkL.PushBack({ 128,768,32,32 });
+	walkL.PushBack({ 96,768,32,32 });
+	walkL.PushBack({ 64,768,32,32 });
+	walkL.loop = true;
+	walkL.speed = 0.1;
+
+	//walk and punch RIGHT
 	walkPunchR.PushBack({ 0,384,32,32 });
 	walkPunchR.PushBack({ 32,384,32,32 });
 	walkPunchR.PushBack({ 64,384,32,32 });
@@ -140,10 +162,25 @@ void Player::PlayerStartAnims() {
 	walkPunchR.loop = true;
 	walkPunchR.speed = 0.3;
 
-	//player <-
-	playerL.PushBack({ 224, 416, 32, 32 });
+	//walk and punch LEFT
+	walkPunchL.PushBack({ 224,800,32,32 });
+	walkPunchL.PushBack({ 192,800,32,32 });
+	walkPunchL.PushBack({ 160,800,32,32 });
+	walkPunchL.PushBack({ 128,800,32,32 });
+	walkPunchL.PushBack({ 96,800,32,32 });
+	walkPunchL.PushBack({ 64,800,32,32 });
+	walkPunchL.loop = true;
+	walkPunchL.speed = 0.3;
 
-	//attack <-
+	//attack RIGHT
+	punchR.PushBack({ 0,32,32,32 });
+	punchR.PushBack({ 32,32,32,32 });
+	punchR.PushBack({ 64,32,32,32 });
+	punchR.PushBack({ 96,32,32,32 });
+	punchR.loop = false;
+	punchR.speed = 0.1;
+
+	//attack LEFT
 	punchL.PushBack({ 224,448,32,32 });
 	punchL.PushBack({ 192,448,32,32 });
 	punchL.PushBack({ 160,448,32,32 });
@@ -151,7 +188,17 @@ void Player::PlayerStartAnims() {
 	punchL.loop = false;
 	punchL.speed = 0.1;
 
-	//doble attack <-
+	//doble attack RIGHT
+	doublePunchR.PushBack({ 0,64,32,32 });
+	doublePunchR.PushBack({ 32,64,32,32 });
+	doublePunchR.PushBack({ 64,64,32,32 });
+	doublePunchR.PushBack({ 96,64,32,32 });
+	doublePunchR.PushBack({ 128,64,32,32 });
+	doublePunchR.PushBack({ 160,64,32,32 });
+	doublePunchR.loop = true;
+	doublePunchR.speed = 0.1;
+
+	//doble attack LEFT
 	doublePunchL.PushBack({ 224,480,32,32 });
 	doublePunchL.PushBack({ 192,480,32,32 });
 	doublePunchL.PushBack({ 160,480,32,32 });
@@ -161,7 +208,7 @@ void Player::PlayerStartAnims() {
 	doublePunchL.loop = true;
 	doublePunchL.speed = 0.1;
 
-	//morir <-
+	//morir LEFT
 	deathL.PushBack({ 224,544,32,32 });
 	deathL.PushBack({ 192,544,32,32 });
 	deathL.PushBack({ 160,544,32,32 });
@@ -173,7 +220,15 @@ void Player::PlayerStartAnims() {
 	deathL.loop = true;
 	deathL.speed = 0.3;
 
-	//hit <-
+	// hit RIGHT ---
+	hitR.PushBack({ 0,160,32,32 });
+	hitR.PushBack({ 32,160,32,32 });
+	hitR.PushBack({ 64,160,32,32 });
+	hitR.PushBack({ 96,160,32,32 });
+	hitR.loop = true;
+	hitR.speed = 0.3;
+
+	//hit LEFT
 	hitL.PushBack({ 224,576,32,32 });
 	hitL.PushBack({ 192,576,32,32 });
 	hitL.PushBack({ 160,576,32,32 });
@@ -181,77 +236,24 @@ void Player::PlayerStartAnims() {
 	hitL.loop = true;
 	hitL.speed = 0.3;
 
-	//idle <-
-	idleL.PushBack({ 224,608,32,32 });
-	idleL.PushBack({ 192,608,32,32 });
-	idleL.PushBack({ 160,608,32,32 });
-	idleL.PushBack({ 128,608,32,32 });
-	idleL.loop = true;
-	idleL.speed = 0.1;
+	//jump RIGHT
+	jumpR.PushBack({ 0,224,32,32 });
+	jumpR.PushBack({ 32,224,32,32 });
+	jumpR.PushBack({ 64,224,32,32 });
+	jumpR.PushBack({ 96,224,32,32 });
+	jumpR.PushBack({ 128,224,32,32 });
+	jumpR.loop = false;
+	jumpR.speed = 0.1;
 
-	//jump <-
+	//jump LEFT
 	jumpL.PushBack({ 224,640,32,32 });
 	jumpL.PushBack({ 192,640,32,32 });
 	jumpL.PushBack({ 160,640,32,32 });
 	jumpL.PushBack({ 128,640,32,32 });
 	jumpL.PushBack({ 96,640,32,32 });
 	jumpL.loop = true;
-	jumpL.speed = 0.3;
+	jumpL.speed = 0.1;
 
-
-	fallL.PushBack({ 64,640,32,32 });
-	fallL.PushBack({ 32,640,32,32 });
-	fallL.PushBack({ 0,640,32,32 });
-	fallL.loop = true;
-	fallL.speed = 0.3;
-
-	//push <-
-	pushL.PushBack({ 224,672,32,32 });
-	pushL.PushBack({ 192,672,32,32 });
-	pushL.PushBack({ 160,672,32,32 });
-	pushL.PushBack({ 128,672,32,32 });
-	pushL.PushBack({ 96,672,32,32 });
-	pushL.PushBack({ 64,672,32,32 });
-	pushL.loop = true;
-	pushL.speed = 0.3;
-
-	//run <-
-	runL.PushBack({ 224,704,32,32 });
-	runL.PushBack({ 192,704,32,32 });
-	runL.PushBack({ 160,704,32,32 });
-	runL.PushBack({ 128,704,32,32 });
-	runL.PushBack({ 96,704,32,32 });
-	runL.PushBack({ 64,704,32,32 });
-	runL.loop = true;
-	runL.speed = 0.2;
-
-	//throw <-
-	throwL.PushBack({ 224,736,32,32 });
-	throwL.PushBack({ 192,736,32,32 });
-	throwL.PushBack({ 160,736,32,32 });
-	throwL.PushBack({ 128,736,32,32 });
-	throwL.loop = true;
-	throwL.speed = 0.3;
-
-	//Walk <- animation
-	walkL.PushBack({ 224,768,32,32 });
-	walkL.PushBack({ 192,768,32,32 });
-	walkL.PushBack({ 160,768,32,32 });
-	walkL.PushBack({ 128,768,32,32 });
-	walkL.PushBack({ 96,768,32,32 });
-	walkL.PushBack({ 64,768,32,32 });
-	walkL.loop = true;
-	walkL.speed = 0.1;
-
-	//walk and punch <-
-	walkPunchL.PushBack({ 224,800,32,32 });
-	walkPunchL.PushBack({ 192,800,32,32 });
-	walkPunchL.PushBack({ 160,800,32,32 });
-	walkPunchL.PushBack({ 128,800,32,32 });
-	walkPunchL.PushBack({ 96,800,32,32 });
-	walkPunchL.PushBack({ 64,800,32,32 });
-	walkPunchL.loop = true;
-	walkPunchL.speed = 0.3;
 }
 
 bool Player::Start()
@@ -299,7 +301,7 @@ bool Player::Start()
 
 bool Player::PreUpdate() {
 	
-	if (app->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) {
+	if (app->input->GetKey(SDL_SCANCODE_F10) == KEY_DOWN) {
 		godMode = !godMode;
 	}
 	return true;
@@ -401,6 +403,19 @@ bool Player::Update(float dt)
 		if (canJump == true && app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
 			playerBody->body->ApplyForceToCenter(b2Vec2(0, -250), 1);
 			canJump = false;
+
+			if (Player_Dir == true) {
+				if (currentAnimation != &jumpR)
+				{
+					jumpR.Reset();
+					currentAnimation = &jumpR;
+				}
+				else if(currentAnimation != &jumpL)
+				{
+					jumpL.Reset();
+					currentAnimation = &jumpL;
+				}
+			}
 		}
 		else {
 			if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && Player_Dir == true) {
@@ -443,19 +458,19 @@ void Player::GodMode() {
 	// --- GOD MODE ---
 	if ((app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT))
 	{
-		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x + 0.1f, playerBody->body->GetPosition().y),playerBody->body->GetAngle());
+		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x + 0.4f, playerBody->body->GetPosition().y),playerBody->body->GetAngle());
 	}
 	if ((app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT))
 	{
-		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x - 0.1f, playerBody->body->GetPosition().y), playerBody->body->GetAngle());
+		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x - 0.4f, playerBody->body->GetPosition().y), playerBody->body->GetAngle());
 	}
 	if ((app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)) 
 	{
-		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x, playerBody->body->GetPosition().y + -0.1f), playerBody->body->GetAngle());
+		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x, playerBody->body->GetPosition().y + -0.4f), playerBody->body->GetAngle());
 	}
 	if ((app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT)) 
 	{
-		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x, playerBody->body->GetPosition().y + 0.1f), playerBody->body->GetAngle());
+		playerBody->body->SetTransform(b2Vec2(playerBody->body->GetPosition().x, playerBody->body->GetPosition().y + 0.4f), playerBody->body->GetAngle());
 	}
 }
 
@@ -468,7 +483,7 @@ bool Player::PostUpdate() {
 		&& app->input->GetKey(SDL_SCANCODE_D) == KEY_IDLE
 		&& app->input->GetKey(SDL_SCANCODE_A) == KEY_IDLE
 		&& app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_IDLE
-		&& currentAnimation != &punchL && currentAnimation != &punchR))
+		&& currentAnimation != &punchL && currentAnimation != &punchR && currentAnimation != &jumpL && currentAnimation != &jumpR))
 	{
 		if (currentAnimation != &idleR && Player_Dir == true)
 		{
@@ -499,6 +514,21 @@ bool Player::PostUpdate() {
 	}
 	else {
 		punchR.Update();
+	}
+	// Jump anim:
+	if (currentAnimation == &jumpL && currentAnimation->GetCurrentFrameINT() == 5) {
+		idleR.Reset();
+		currentAnimation = &idleR;
+	}
+	else {
+		jumpL.Update();
+	}
+	if (currentAnimation == &jumpR && currentAnimation->GetCurrentFrameINT() == 5) {
+		idleR.Reset();
+		currentAnimation = &idleR;
+	}
+	else {
+		jumpR.Update();
 	}
 
 	// Update Animation each frame
